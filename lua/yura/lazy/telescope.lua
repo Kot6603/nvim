9cddfaf06d14436235d4367eb814c5000a6de2df
kot <89110272+Kot6603@users.noreply.github.com>
2024-01-13 22:01:58 +1300

    fix: removed fzf as a loaded extension with telescope

--- lua/yura/lazy/telescope.lua
+++ lua/yura/lazy/telescope.lua
@@ -1,24 +1,24 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		config = function()
-			local telescope = require("telescope")
-			local actions = require("telescope.actions")
-			-- configure telescope
-			telescope.setup({
-				-- configure custom mappings
-				defaults = {
-					mappings = {
-						i = {
-							["<C-k>"] = actions.move_selection_previous, -- move to prev result
-							["<C-j>"] = actions.move_selection_next, -- move to next result
-							["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-						},
-					},
-				},
-			})
-			telescope.load_extension("fzf")
-		end,
-	},
-	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+  {
+    "nvim-telescope/telescope.nvim",
+    config = function()
+      local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      -- configure telescope
+      telescope.setup({
+        -- configure custom mappings
+        defaults = {
+          mappings = {
+            i = {
+              ["<C-k>"] = actions.move_selection_previous,                -- move to prev result
+              ["<C-j>"] = actions.move_selection_next,                    -- move to next result
+              ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+            },
+          },
+        },
+      })
+    end,
+  },
+  { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
 }
+
