1128527654f9b3b6b53df27b8632708e9250c630
Kot <89110272+Kot6603@users.noreply.github.com>
2024-02-05 23:42:58 +1300

    Added :Git blame and :Git log shortcuts

--- lua/yura/lazy/fugitive.lua
+++ lua/yura/lazy/fugitive.lua
@@ -2,5 +2,7 @@ return {
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gl", "<Cmd>Git log<CR>")
+		vim.keymap.set("n", "<leader>gw", "<Cmd>Git blame<CR>")
 	end,
 }
